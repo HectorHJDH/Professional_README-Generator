a2ce9ffdf939279f9c89604f25fa54ed8101d2c1
HectorJosueDH <hectorjosue78@gmail.com>
2023-05-15 17:49:18 -0600

    Added questions and possible README generation

---
+++ Develop/README.md
@@ -0,0 +1,32 @@
+
+# Jo
+![GNU GPLv3 license](https://img.shields.io/badge/license-GNU GPLv3-green)
+
+## Description
+a
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+a
+
+## Usage
+a
+
+## License
+This project is licensed under the GNU GPLv3 license.
+
+## Contributing
+MDMAS
+
+## Tests
+asdkasmdakds
+
+## Questions
+If you have any questions or issues, please contact me directly at dasd. You can also find more of my work at https://github.com/sa.
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,85 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'Please introduce the Title of your project:'
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Please write the description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'Table of Contents',
+        message: 'Please introduce the table of contents of the project:'
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'Introduce installation steps:'
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Introduce an usage description:'
+    },
+    {
+        type: 'list',
+        name: 'License',
+        choices: [
+            'Apache', 
+            'Creative Commons', 
+            'BSD', 
+            'Eclipse',
+            'GNU',
+            'IBM',
+            'ISC',
+            'MIT',
+            'Mozilla',
+            'Open Data Commons',
+            'Perl',
+            'Unlicense',
+            'WTFPL',
+            'Zlib'
+        ],
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'Write the contributors:'
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'Write the tests:'
+    },
+    {
+        type: 'input',
+        name: 'Questions',
+        message: 'Write the question:'
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+
+    fs.writeFile("README.md", data, (err) => {
+      if (err) throw err;
+      console.log("README.md file has been created successfully!");
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+  .prompt(questions).then(writeToFile(data));
+}
 
 // Function call to initialize app
 init();
